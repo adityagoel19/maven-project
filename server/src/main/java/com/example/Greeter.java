b837dfd05a52dbb82962556ba640da427349e6f3
Aditya Goel <aditya@fitphilia.com>
2018-03-13 21:27:43 +0530

    warnings removed

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,10 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   * Sample comment line
+   */
+  public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
